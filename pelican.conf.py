d9b63ce312c6853a3ee31e4d973237204be86ae0
Jessie Anderson <housewifehacker@gmail.com>
2012-06-11 23:39:31 -0400

    added the files needed to generate the blog

---
+++ pelican.conf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Jessie Anderson"
+SITENAME = u"housewife_hacker"
+SITEURL = 'http://housewifehacker.com'
+TIMEZONE = 'America/Santiago'
+DEFAULT_DATE_FORMAT = '%B %Y'
+DEFAULT_CATEGORY = 'Code'
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+         )
+
+# Social widget
+SOCIAL = (
+          ('Follow Me On Twitter', 'http://twitter.com/housewifehacker'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+
+    
